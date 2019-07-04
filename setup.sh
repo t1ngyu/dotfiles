34dea33bfb09cb12d0f30f575ded89b037669a8d
t1ngyu <229856331@qq.com>
2019-07-04 13:34:08 +0800

    add bash setup.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cd `dirname $0`
 install_dir=installed_files
 
 
-install_vim () {
+setup_vim () {
     echo setup vim...
     # Get vundle
     git clone https://github.com/VundleVim/Vundle.vim.git $install_dir/vim/bundle/Vundle.vim
@@ -23,10 +23,10 @@ install_vim () {
     ln -sf `pwd`/$install_dir/vim ~/.vim
 
     # Install plugins
-    vim +PluginInstall +qall
+    #vim +PluginInstall +qall
 }
 
-install_tmux () {
+setup_tmux () {
     echo setup tmux...
     if [ -e ~/.tmux.conf ]
     then
@@ -34,6 +34,12 @@ install_tmux () {
     fi
     ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
 }
+
+setup_bash () {
+    echo setup bash...
+    echo . `pwd`/bash/bashrc >> ~/.bashrc
+}
+
 
 if [ -e $install_dir ]
 then
@@ -49,6 +55,7 @@ fi
 
 
 mkdir $install_dir
-install_vim
-install_tmux
+setup_vim
+setup_tmux
+setup_bash
 
